86a1c9ef92e35db90fda4f231039c65571931a9f
Lavanya Livisarani <jayasikothalawala@gmail.com>
2026-03-25 17:48:33 +0530

    Project Initiated

---
+++ src/test/java/com/Campus_Hub/Smart_Campus_Operations_Hub/SmartCampusOperationsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Campus_Hub.Smart_Campus_Operations_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusOperationsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
